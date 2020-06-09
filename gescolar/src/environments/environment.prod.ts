fa54e4bf9c6898ffbc075ddc1103aac95f34531f
Francisco D' Avila Alves <francisco.d.alves@gmail.com>
2020-06-09 01:14:27 -0300

    efsf
    fdsdf

--- gescolar/src/environments/environment.prod.ts
+++ gescolar/src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
+
 export const environment = {
-  production: true,
-  apiUrl: 'https://algamoney-api.herokuapp.com',
-  fotoAlunoDefault: 'https://s3-sa-east-1.amazonaws.com/gescolar/232f56b8-0a40-41a4-a712-2733fa80fcf8_.jpg',
-  fotoProfessor: 'https://s3-sa-east-1.amazonaws.com/gescolar/c55f57dd-ee54-41f1-9024-2128d1913b39_.jpg',
-  tokenWhitelistedDomains: [new RegExp('algamoney-api.herokuapp.com') ],
-  tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
-};
+    production: false,
+    apiUrl: 'http://localhost:8080',
+    fotoAlunoDefault: 'https://gescolar.s3-sa-east-1.amazonaws.com/39812a77-b94a-401e-8ccd-539905f3ff03_.jpg',
+    fotoProfessor: 'https://gescolar.s3-sa-east-1.amazonaws.com/08860cd0-72d8-4939-8700-d6d6ca88f53d_.jpg',
+    tokenWhitelistedDomains: [new RegExp('localhost:8080') ],
+    tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
+  };
